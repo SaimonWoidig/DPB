56879c1341d88d5ebc8ce1719843f24ad2b5d8d5
Šimon Woidig <woidig.simon@gmail.com>
2024-04-18 17:20:12 +0200

    finish work on bonus

--- cv07/go.mod
+++ cv07/go.mod
@@ -2,4 +2,7 @@ module github.com/SaimonWoidig/DPB/cv07
 
 go 1.22.1
 
-require github.com/opensearch-project/opensearch-go/v4 v4.0.0
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/opensearch-project/opensearch-go/v4 v4.0.0
+)
--- cv07/main.go
+++ cv07/main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
-	"net/http"
+	"os"
+	"strings"
 
+	"github.com/joho/godotenv"
 	"github.com/opensearch-project/opensearch-go/v4"
 	"github.com/opensearch-project/opensearch-go/v4/opensearchapi"
 	"github.com/opensearch-project/opensearch-go/v4/opensearchutil"
 )
 
-const OPENSEARCH_ADMIN_PASSWORD string = "ThisIsASecret123" // used only for local development
-const INDEX_NAME string = "person"
+const (
+	EnvOpensearchUrl      string = "OPENSEARCH_URL"
+	EnvOpensearchUser     string = "OPENSEARCH_USER"
+	EnvOpensearchPassword string = "OPENSEARCH_PASSWORD"
+)
+
+const IndexName string = "person"
 
 type Person struct {
 	Name    string `json:"name"`
@@ -20,15 +26,30 @@ type Person struct {
 }
 
 func main() {
-	fmt.Println("Connecting to Opensearch")
+	if err := godotenv.Load(".env"); err != nil {
+		panic(err.Error())
+	}
+
+	// load configuration parameters from dotenv
+	url := os.Getenv(EnvOpensearchUrl)
+	if len(url) == 0 {
+		panic("OPENSEARCH_URL is unset")
+	}
+	username := os.Getenv(EnvOpensearchUser)
+	if len(username) == 0 {
+		panic("OPENSEARCH_USER is unset")
+	}
+	password := os.Getenv(EnvOpensearchPassword)
+	if len(password) == 0 {
+		panic("OPENSEARCH_PASSWORD is unset")
+	}
+
+	fmt.Printf("Connecting to Opensearch at %q\n", url)
 	client, err := opensearchapi.NewClient(opensearchapi.Config{
 		Client: opensearch.Config{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			},
-			Addresses: []string{"https://localhost:9200"},
-			Username:  "admin",
-			Password:  OPENSEARCH_ADMIN_PASSWORD,
+			Addresses: []string{url},
+			Username:  username,
+			Password:  password,
 		},
 	})
 	if err != nil {
@@ -41,7 +62,9 @@ func main() {
 	fmt.Printf("Cluster INFO:\n  Cluster Name: %s\n  Cluster UUID: %s\n  Version Number: %s\n", infoRes.ClusterName, infoRes.ClusterUUID, infoRes.Version.Number)
 
 	// create index
-	createIndexReq := opensearchapi.IndicesCreateReq{Index: INDEX_NAME}
+	createIndexReq := opensearchapi.IndicesCreateReq{
+		Index: IndexName,
+	}
 	createIndexRes, err := client.Indices.Create(context.Background(), createIndexReq)
 	if err != nil {
 		panic(err.Error())
@@ -52,9 +75,12 @@ func main() {
 	newPersonId := "123"
 	newPerson := Person{Name: "John", Surname: "Doe"}
 	newPersonReq := opensearchapi.IndexReq{
-		Index:      INDEX_NAME,
+		Index:      IndexName,
 		Body:       opensearchutil.NewJSONReader(&newPerson),
 		DocumentID: newPersonId,
+		Params: opensearchapi.IndexParams{
+			Refresh: "true",
+		},
 	}
 	newPersonRes, err := client.Index(context.Background(), newPersonReq)
 	if err != nil {
@@ -63,7 +89,10 @@ func main() {
 	fmt.Printf("Created person - id: %v\n", newPersonRes.ID)
 
 	// print created person
-	getPersonReq := opensearchapi.DocumentGetReq{Index: INDEX_NAME, DocumentID: newPersonId}
+	getPersonReq := opensearchapi.DocumentGetReq{
+		Index:      IndexName,
+		DocumentID: newPersonId,
+	}
 	getPersonRes, err := client.Document.Get(context.Background(), getPersonReq)
 	if err != nil {
 		panic(err.Error())
@@ -74,8 +103,54 @@ func main() {
 	}
 	fmt.Printf("Person - id: %v, data: %v\n", getPersonRes.ID, string(data))
 
+	// update person
+	updatePersonReq := opensearchapi.UpdateReq{
+		Index:      IndexName,
+		DocumentID: newPersonId,
+		Body:       strings.NewReader(`{"doc": {"name": "Bob"}}`),
+		Params: opensearchapi.UpdateParams{
+			Refresh: "true",
+		},
+	}
+	updatePersonRes, err := client.Update(context.Background(), updatePersonReq)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Updated person - id: %v, status %v\n", updatePersonRes.ID, updatePersonRes.Result)
+
+	// print all in index person
+	getAllPersonReq := opensearchapi.SearchReq{
+		Indices: []string{IndexName},
+		Body:    strings.NewReader(`{"query": {"match_all": {}}}`),
+	}
+	getAllPersonRes, err := client.Search(context.Background(), &getAllPersonReq)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Found:")
+	for _, hit := range getAllPersonRes.Hits.Hits {
+		data, err := hit.Source.MarshalJSON()
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("  - id: %v, data: %v\n", hit.ID, string(data))
+	}
+
+	// delete person
+	deletePersonReq := opensearchapi.DocumentDeleteReq{
+		Index:      IndexName,
+		DocumentID: newPersonId,
+	}
+	deletePersonRes, err := client.Document.Delete(context.Background(), deletePersonReq)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Deleted person - id: %v, status %v\n", deletePersonRes.ID, deletePersonRes.Result)
+
 	// delete index
-	deleteIndexReq := opensearchapi.IndicesDeleteReq{Indices: []string{INDEX_NAME}}
+	deleteIndexReq := opensearchapi.IndicesDeleteReq{
+		Indices: []string{IndexName},
+	}
 	deleteIndexRes, err := client.Indices.Delete(context.Background(), deleteIndexReq)
 	if err != nil {
 		panic(err.Error())
