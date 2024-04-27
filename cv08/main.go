217bb60f9bb6c769813220d10f2b83b07c6e2f07
Šimon Woidig <woidig.simon@gmail.com>
2024-04-27 14:19:49 +0200

    added data import utility

--- cv08/main.go
+++ cv08/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
@@ -17,8 +18,11 @@ const (
 )
 
 const IndexOrders string = "orders"
+const DatafileOrders string = "data/orders-bulk.json"
 const IndexProducts string = "products"
+const DatafileProducts string = "data/products-bulk.json"
 const IndexRecipes string = "recipes"
+const DatafileRecipes string = "data/recipes-bulk.json"
 
 type Person struct {
 	Name    string `json:"name"`
@@ -61,6 +65,14 @@ func main() {
 	}
 	fmt.Printf("Cluster INFO:\n  Cluster Name: %s\n  Cluster UUID: %s\n  Version Number: %s\n", infoRes.ClusterName, infoRes.ClusterUUID, infoRes.Version.Number)
 
+	// delete old indices
+	deleteIndicesReq := opensearchapi.IndicesDeleteReq{Indices: []string{IndexProducts, IndexOrders, IndexRecipes}}
+	deleteIndicesResp, err := client.Indices.Delete(context.Background(), deleteIndicesReq)
+	if err != nil {
+		fmt.Printf("Error deleting indices: %v\n", err.Error())
+	} else {
+		fmt.Printf("Indices deleted - ack: %v\n", deleteIndicesResp.Acknowledged)
+	}
 	// create indices
 	var createIndexReq opensearchapi.IndicesCreateReq
 	var createIndexResp *opensearchapi.IndicesCreateResp
@@ -91,4 +103,44 @@ func main() {
 		panic(err.Error())
 	}
 	fmt.Printf("Recipes index created - ack: %v, idx: %v\n", createIndexResp.Acknowledged, createIndexResp.Index)
+
+	// import data
+	// products
+	productsData, err := os.ReadFile(DatafileProducts)
+	if err != nil {
+		panic(err.Error())
+	}
+	productsDataBuf := bytes.NewBuffer(productsData)
+	bulkProductsReq := opensearchapi.BulkReq{Index: IndexProducts, Body: productsDataBuf, Params: opensearchapi.BulkParams{Refresh: "true"}}
+	bulkProductsResp, err := client.Bulk(context.Background(), bulkProductsReq)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Bulk: %v products imported (took: %vms)\n", len(bulkProductsResp.Items), bulkProductsResp.Took)
+	// orders
+	ordersData, err := os.ReadFile(DatafileOrders)
+	if err != nil {
+		panic(err.Error())
+	}
+	ordersDataBuf := bytes.NewBuffer(ordersData)
+	bulkOrdersReq := opensearchapi.BulkReq{Index: IndexOrders, Body: ordersDataBuf, Params: opensearchapi.BulkParams{Refresh: "true"}}
+	bulkOrdersResp, err := client.Bulk(context.Background(), bulkOrdersReq)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Bulk: %v orders imported (took: %vms)\n", len(bulkOrdersResp.Items), bulkOrdersResp.Took)
+	// recipes
+	recipesData, err := os.ReadFile(DatafileRecipes)
+	if err != nil {
+		panic(err.Error())
+	}
+	recipesDataBuf := bytes.NewBuffer(recipesData)
+	bulkRecipesReq := opensearchapi.BulkReq{Index: IndexRecipes, Body: recipesDataBuf, Params: opensearchapi.BulkParams{Refresh: "true"}}
+	bulkRecipesResp, err := client.Bulk(context.Background(), bulkRecipesReq)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("Bulk: %v recipes imported (took: %vms)\n", len(bulkRecipesResp.Items), bulkRecipesResp.Took)
+
+	fmt.Println("Done. Indexes created and data imported.")
 }
